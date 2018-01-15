01875ea42841ded583047cd870acb111cfd5effe
NProof <34260623+NProof@users.noreply.github.com>
2018-01-15 18:59:57 +0800

    #define

--- Banker's algorithm/System.h
+++ Banker's algorithm/System.h
@@ -1,6 +1,15 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <set>
+#include "Process.h"
+#include "Resource.h"
 
 class System{
 	//Safety Algorithm
 	//Resource-Request Algorithm
 	//Deadlock-Detection Algorithm
+	std::set<Process> P;
 };
+
+#endif
