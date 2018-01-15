ab5f2b0d41e68d235a267c3db3e1e4f2f455fa4a
NProof <34260623+NProof@users.noreply.github.com>
2018-01-15 21:58:38 +0800

    update ^

--- Banker's algorithm/System.h
+++ Banker's algorithm/System.h
@@ -18,48 +18,49 @@ public :
 		resourceSet.insert(r);
 	}
 	
-	//Safety Algorithm
-	bool safety(){
-		std::map<Resource*, unsigned int> work ;
-		std::map<Process*, bool> finish ;
-		for(std::set<Resource*>::iterator rit=resourceSet.begin(); rit!=resourceSet.end(); ++rit){
-			work[*rit] = (*rit)->getAvailable();
-		}
-		for(std::set<Process*>::iterator pit=processSet.begin(); pit!=processSet.end(); ++pit){
-			finish[*pit] = false;
-		}
-		bool finded;
-		do {
-			finded = false;
-			for(std::set<Process*>::iterator pit=processSet.begin(); pit!=processSet.end(); ++pit){
-				if(!finish[*pit]){
-					bool all = true;
-					for(std::set<Resource*>::iterator rit=resourceSet.begin(); rit!=resourceSet.end(); ++rit){
-						if(!( (*pit)->need(*rit) <= work[*rit] )){
-							all = false;
-							break;
-						}
-					}
-					if(all){
-						for(std::set<Resource*>::iterator rit=resourceSet.begin(); rit!=resourceSet.end(); ++rit){
-							work[*rit] += (*pit)->allocation[*rit];
-						}
-						finded = true;
-						finish[*pit] = true;
-					}
-				}
-			}
-		} while(finded);
-		for(std::set<Process*>::iterator pit=processSet.begin(); pit!=processSet.end(); ++pit){
-			if(!finish[*pit]){
-				return false;
-			}
-		}
-		return true;
-	}
+	bool safety(); // Safety Algorithm
 	
 	//Resource-Request Algorithm
 	//Deadlock-Detection Algorithm
 };
+
+bool System::safety(){
+	std::map<Resource*, unsigned int> work ;
+	std::map<Process*, bool> finish ;
+	for(std::set<Resource*>::iterator rit=resourceSet.begin(); rit!=resourceSet.end(); ++rit){
+		work[*rit] = (*rit)->getAvailable();
+	}
+	for(std::set<Process*>::iterator pit=processSet.begin(); pit!=processSet.end(); ++pit){
+		finish[*pit] = false;
+	}
+	bool finded;
+	do {
+		finded = false;
+		for(std::set<Process*>::iterator pit=processSet.begin(); pit!=processSet.end(); ++pit){
+			if(!finish[*pit]){
+				bool all = true;
+				for(std::set<Resource*>::iterator rit=resourceSet.begin(); rit!=resourceSet.end(); ++rit){
+					if(!( (*pit)->need(*rit) <= work[*rit] )){
+						all = false;
+						break;
+					}
+				}
+				if(all){
+					for(std::set<Resource*>::iterator rit=resourceSet.begin(); rit!=resourceSet.end(); ++rit){
+						work[*rit] += (*pit)->allocation[*rit];
+					}
+					finded = true;
+					finish[*pit] = true;
+				}
+			}
+		}
+	} while(finded);
+	for(std::set<Process*>::iterator pit=processSet.begin(); pit!=processSet.end(); ++pit){
+		if(!finish[*pit]){
+			return false;
+		}
+	}
+	return true;
+}
 
 #endif
