a798c58d7b7793e6563f925facec093999bcc08a
NProof <34260623+NProof@users.noreply.github.com>
2018-01-15 18:07:48 +0800

    init

---
+++ Banker's algorithm/System.h
@@ -0,0 +1,6 @@
+
+class System{
+	//Safety Algorithm
+	//Resource-Request Algorithm
+	//Deadlock-Detection Algorithm
+};
---
+++ Banker's algorithm/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "System.h"
+#include "Process.h"
+#include "Resource.h"
+
+using namespace std;
+
+int main(int argc, char **argv){
+	System S();
+	Process p();
+	Resource R();
+	return 0;
+}
